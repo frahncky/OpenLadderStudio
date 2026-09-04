a736678a5ec2e2609e779ed60ddf2f8e80d70c0d
Francisco S. Viana <31813565+frahncky@users.noreply.github.com>
2026-09-03 21:56:06 -0300

    Atualizar iniciador para OpenLadder Studio

--- PC12_v2.1_Windows7_v3_portatil/INICIAR_PC12.bat
+++ PC12_v2.1_Windows7_v3_portatil/INICIAR_PC12.bat
@@ -4,10 +4,10 @@ cd /d "%~dp0"
 
 if exist "%~dp0BUILD_INTERFACE_MODERNA.bat" call "%~dp0BUILD_INTERFACE_MODERNA.bat"
 
-if not exist "%~dp0PC12_Studio.exe" (
-    echo ERRO: PC12_Studio.exe nao foi gerado.
+if not exist "%~dp0OpenLadderStudio.exe" (
+    echo ERRO: OpenLadderStudio.exe nao foi gerado.
     pause
     exit /b 1
 )
 
-start "" "%~dp0PC12_Studio.exe"
+start "" "%~dp0OpenLadderStudio.exe"
