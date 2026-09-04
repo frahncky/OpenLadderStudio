7eaa589541dec0a5bc27ddaf2168c61c45b4e3ab
Francisco S. Viana <31813565+frahncky@users.noreply.github.com>
2026-09-03 23:31:36 -0300

    Corrigir inicializador do Editor Ladder

--- PC12_v2.1_Windows7_v3_portatil/INICIAR_EDITOR_LADDER.bat
+++ PC12_v2.1_Windows7_v3_portatil/INICIAR_EDITOR_LADDER.bat
@@ -2,17 +2,17 @@
 setlocal
 cd /d "%~dp0"
 
-echo Atualizando PC12 Ladder Studio...
+echo Atualizando OpenLadder Studio - Editor Ladder...
 call "BUILD_INTERFACE_MODERNA.bat"
 if errorlevel 1 (
     echo.
-    echo Nao foi possivel atualizar o Ladder Studio.
-    if exist "PC12_Ladder.exe" (
+    echo Nao foi possivel atualizar o Editor Ladder.
+    if exist "OpenLadderEditor.exe" (
         echo Abrindo a ultima versao compilada disponivel.
-        start "" "%~dp0PC12_Ladder.exe"
+        start "" "%~dp0OpenLadderEditor.exe"
         exit /b 0
     )
     exit /b 1
 )
 
-start "" "%~dp0PC12_Ladder.exe"
+start "" "%~dp0OpenLadderEditor.exe"
