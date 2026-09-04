756b91372d813dd81c0beb965f7d76e75ec5239a
Francisco S. Viana <31813565+frahncky@users.noreply.github.com>
2026-09-03 21:44:33 -0300

    Abrir somente o PC12 Studio novo

--- PC12_v2.1_Windows7_v3_portatil/INICIAR_PC12.bat
+++ PC12_v2.1_Windows7_v3_portatil/INICIAR_PC12.bat
@@ -2,22 +2,12 @@
 setlocal
 cd /d "%~dp0"
 
-rem Tenta recompilar para garantir que a interface corresponde ao codigo atual.
-rem Se a compilacao falhar, ainda usamos o ultimo executavel valido existente.
-if exist "%~dp0BUILD_INTERFACE_MODERNA.bat" (
-    call "%~dp0BUILD_INTERFACE_MODERNA.bat"
+if exist "%~dp0BUILD_INTERFACE_MODERNA.bat" call "%~dp0BUILD_INTERFACE_MODERNA.bat"
+
+if not exist "%~dp0PC12_Studio.exe" (
+    echo ERRO: PC12_Studio.exe nao foi gerado.
+    pause
+    exit /b 1
 )
 
-if exist "%~dp0PC12_Studio.exe" (
-    start "" "%~dp0PC12_Studio.exe"
-    exit /b 0
-)
-
-rem Fallback para a central moderna anterior
-if exist "%~dp0PC12_Moderno.exe" (
-    start "" "%~dp0PC12_Moderno.exe"
-    exit /b 0
-)
-
-rem Fallback final: software legado
-start "" "%~dp0pc12.exe"
+start "" "%~dp0PC12_Studio.exe"
